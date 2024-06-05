d0be4dc33d29135295563deb49d6e7ed8baa16eb
Amandeep Singh <amandeepsingh@emerson.com>
2024-06-05 17:03:35 +0530

    Responsive images

--- components/PhotosList.jsx
+++ components/PhotosList.jsx
@@ -3,12 +3,20 @@ import Link from "next/link";
 import React from "react";
 
 const PhotosList = ({ photos }) => {
-  console.log(photos);
   return (
-    <ul className="pl-0 menu menu-vertical">
+    <ul className="grid gap-6 mt-6 sm:grid-cols-2">
       {photos.map((photo) => (
         <li key={photo.id}>
           <Link href={`/photos/${photo.id}`} className="text-xl font-medium">
+            <div className="relative h-48 mb-4">
+              <Image
+                src={photo.thumbnail}
+                fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
+                alt={photo.title}
+                className="object-cover rounded-md shadow-lg bg-slate-700"
+              />
+            </div>
             {photo.title}
           </Link>
         </li>
@@ -16,5 +24,28 @@ const PhotosList = ({ photos }) => {
     </ul>
   );
 };
+
+// const PhotosList = ({ photos }) => {
+//   return (
+//     <ul className="pl-0 menu menu-vertical">
+//       {photos.map((photo) => (
+//         <li key={photo.id}>
+//           <Link href={`/photos/${photo.id}`} className="text-xl font-medium">
+//             <div className="relative h-48 mb-4">
+//               <Image
+//                 src={photo.thumbnail}
+//                 fill
+//                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
+//                 alt={photo.title}
+//                 className="object-cover rounded-lg"
+//               />
+//             </div>
+//             {photo.title}
+//           </Link>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
 
 export default PhotosList;
