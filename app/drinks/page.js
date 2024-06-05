bb1974926dd298944aa63a2017dede5add1fb60c
Amandeep Singh <amandeepsingh@emerson.com>
2024-06-05 10:13:45 +0530

    Fetch data in server components

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,6 +1,12 @@
 import React from "react";
 
-const Drinks = () => {
+const url = "https://jsonplaceholder.typicode.com/photos";
+
+const Drinks = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+
   return <div>Drinks page</div>;
 };
 
