4835dc7132d740a4c7ef31117df4494e907e8484
Amandeep Singh <amandeepsingh@emerson.com>
2024-06-10 15:11:56 +0530

    Server actions

--- components/TaskForm.jsx
+++ components/TaskForm.jsx
@@ -1,7 +1,33 @@
+import prisma from "@/utils/db";
+import { revalidatePath } from "next/cache";
 import React from "react";
 
+const createTask = async (formData) => {
+  "use server";
+  const content = formData.get("content");
+  await prisma.task.create({
+    data: { content },
+  });
+  revalidatePath("/tasks");
+};
+
 const TaskForm = () => {
-  return <div className="text-7xl">TaskForm</div>;
+  return (
+    <form action={createTask}>
+      <div className="w-full join">
+        <input
+          type="text"
+          className="w-full input input-bordered join-item"
+          placeholder="Type here..."
+          name="content"
+          required
+        />
+        <button type="submit" className="btn btn-primary join-item">
+          Create Task
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default TaskForm;
